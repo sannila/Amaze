4be499c7a063dce95c2176465274e4039035b6c0
sanjai <sanjaim@kmitsolutions.com>
2021-12-13 21:25:05 +0530

    updated the Listener.class

--- src/main/java/utils/Listener.java
+++ src/main/java/utils/Listener.java
@@ -5,32 +5,32 @@ import org.testng.*;
 public class Listener implements ITestListener, ISuiteListener {
     @Override
     public void onStart(ISuite iSuite) {
-        System.out.println("Name of the suite starts: " + iSuite.getName());
+        System.out.println("* * * * * * * * * * * * Name of the suite starts: " + iSuite.getName() + " * * * * * * * * * * * * ");
     }
 
     @Override
     public void onFinish(ISuite iSuite) {
-        System.out.println("Name of the suite Finished: " + iSuite.getName());
+        System.out.println("* * * * * * * * Name of the suite Finished: " + iSuite.getName() + " * * * * * * * * ");
     }
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-
+        System.out.println("* * * * * * * * Name of the test start: "+ iTestResult.getName() + " * * * * * * * * ");
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-
+        System.out.println("* * * * * * * * Name of the test success: "+ iTestResult.getName() + " * * * * * * * * ");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-
+        System.out.println("* * * * * * * * Name of the test failure: "+ iTestResult.getName());
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-
+        System.out.println("* * * * * * * * Name of the test skipped: "+ iTestResult.getName() + " * * * * * * * *  ");
     }
 
     @Override
@@ -40,11 +40,11 @@ public class Listener implements ITestListener, ISuiteListener {
 
     @Override
     public void onStart(ITestContext iTestContext) {
-
+        System.out.println("* * * * On start: "+ iTestContext.getName() + " * * * * ");
     }
 
     @Override
     public void onFinish(ITestContext iTestContext) {
-
+        System.out.println("* * * * On Finish: "+ iTestContext.getName() + " * * * * ");
     }
 }
